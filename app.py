739944ae8fc125f355097f7f7ced96b832f8a2c1
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-05-28 14:52:47 +0200

    4-5-2024

--- app.py
+++ app.py
@@ -15,5 +15,14 @@ def item():
 def market():
     return render_template('market.html')
 
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+
+@app.route('/addPrice')
+def addPrice():
+    return render_template('addPrice.html')    
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
